5dbbf5eeaaf76d2b0824d403501659b2f2da9bab
Jean808 <42712570+Jean808@users.noreply.github.com>
2025-04-12 18:43:44 +0200

    premier_test_dqn

--- tests/sandbox.py
+++ tests/sandbox.py
@@ -3,9 +3,9 @@ import time
 import os, sys
 import numpy as np
 import random as rd
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
-sys.path.append(parent_dir)
-from dqn_trainer import DeepQNetwork
+#parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+#sys.path.append(parent_dir)
+#from dqn_trainer import DeepQNetwork
 
 rd.seed(42)
 
@@ -14,9 +14,14 @@ rd.seed(42)
 
 # vec = np.random.random((1, 5))
 vec = np.random.random((16, 5))
-print(vec.shape)
+#print(vec.shape)
 
-dqn = DeepQNetwork()
-print(dqn.model.predict(vec).shape)
+#dqn = DeepQNetwork()
+#print(dqn.model.predict(vec).shape)
 
 
+x_space = np.linspace(0, 1200, 30)
+
+state_x = np.digitize(1000, x_space)
+
+print(state_x)
